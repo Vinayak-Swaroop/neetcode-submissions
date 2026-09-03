b10c0f07445dc85c2bf1ef4a5bcfa8e380d32e5b
Vinayak Swaroop Bhatnagar <70646136+Vinayak-Swaroop@users.noreply.github.com>
2026-09-03 11:02:33 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (j > i) {
+            char right = s.charAt(j);
+            while (i < s.length() - 1
+                && (!Character.isDigit(s.charAt(i)) && !Character.isLetter(s.charAt(i))))
+                i++;
+            while (j > 0 && (!Character.isDigit(s.charAt(j)) && !Character.isLetter(s.charAt(j))))
+                j--;
+            if ((Character.isDigit(s.charAt(i)) || Character.isLetter(s.charAt(i)))
+                && (Character.isDigit(s.charAt(j)) || Character.isLetter(s.charAt(j))))
+                if (Character.toUpperCase(s.charAt(i)) != Character.toUpperCase(s.charAt(j)))
+                    return false;
+                else {
+                    i++;
+                    j--;
+                }
+        }
+        return true;
+    }
+}
