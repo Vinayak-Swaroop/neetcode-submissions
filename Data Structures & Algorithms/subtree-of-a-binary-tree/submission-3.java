91e81fad7b3aae2d94ab4d46a2a79e2235da1bef
Vinayak Swaroop Bhatnagar <70646136+Vinayak-Swaroop@users.noreply.github.com>
2026-09-14 21:59:23 +0530

    Add: subtree-of-a-binary-tree - submission-3

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-3.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        return traverse(root, subRoot);
+    }
+    private boolean traverse(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null)
+            return true;
+        if (node1 == null || node2 == null)
+            return false;
+        if (isSameTree(node1, node2))
+            return true;
+        return traverse(node1.left, node2) || traverse(node1.right, node2);
+    }
+    private boolean isSameTree(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null)
+            return true;
+        if (node1 == null || node2 == null)
+            return false;
+        if (node1.val != node2.val)
+            return false;
+        return isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right);
+    }
+}
